36aa6dfc28fe82d9e652b6b7b0bee5309e0dcb97
CprChandan <chandraprakashr@gmail.com>
2024-11-30 11:22:47 +0530

    Added a service file

---
+++ app/src/main/java/com/cpr/jenkinsdemo/MyIntentService.kt
@@ -0,0 +1,96 @@
+package com.cpr.jenkinsdemo
+
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+
+/**
+ * An [IntentService] subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ *
+
+ */
+// TODO: Rename actions, choose action names that describe tasks that this
+// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+private const val ACTION_FOO = "com.cpr.jenkinsdemo.ui.theme.action.FOO"
+private const val ACTION_BAZ = "com.cpr.jenkinsdemo.ui.theme.action.BAZ"
+
+// TODO: Rename parameters
+private const val EXTRA_PARAM1 = "com.cpr.jenkinsdemo.ui.theme.extra.PARAM1"
+private const val EXTRA_PARAM2 = "com.cpr.jenkinsdemo.ui.theme.extra.PARAM2"
+class MyIntentService : IntentService("MyIntentService") {
+
+
+
+
+    override fun onHandleIntent(intent: Intent?) {
+        when (intent?.action) {
+            ACTION_FOO -> {
+                val param1 = intent.getStringExtra(EXTRA_PARAM1)
+                val param2 = intent.getStringExtra(EXTRA_PARAM2)
+                handleActionFoo(param1, param2)
+            }
+
+            ACTION_BAZ -> {
+                val param1 = intent.getStringExtra(EXTRA_PARAM1)
+                val param2 = intent.getStringExtra(EXTRA_PARAM2)
+                handleActionBaz(param1, param2)
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionFoo(param1: String?, param2: String?) {
+        TODO("Handle action Foo")
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private fun handleActionBaz(param1: String?, param2: String?) {
+        TODO("Handle action Baz")
+    }
+
+    companion object {
+        /**
+         * Starts this service to perform action Foo with the given parameters. If
+         * the service is already performing a task this action will be queued.
+         *
+         * @see IntentService
+         */
+        // TODO: Customize helper method
+        @JvmStatic
+        fun startActionFoo(context: Context, param1: String, param2: String) {
+            val intent = Intent(context, MyIntentService::class.java).apply {
+                action = ACTION_FOO
+                putExtra(EXTRA_PARAM1, param1)
+                putExtra(EXTRA_PARAM2, param2)
+            }
+            context.startService(intent)
+        }
+
+        /**
+         * Starts this service to perform action Baz with the given parameters. If
+         * the service is already performing a task this action will be queued.
+         *
+         * @see IntentService
+         */
+        // TODO: Customize helper method
+        @JvmStatic
+        fun startActionBaz(context: Context, param1: String, param2: String) {
+            val intent = Intent(context, MyIntentService::class.java).apply {
+                action = ACTION_BAZ
+                putExtra(EXTRA_PARAM1, param1)
+                putExtra(EXTRA_PARAM2, param2)
+            }
+            context.startService(intent)
+        }
+    }
+}

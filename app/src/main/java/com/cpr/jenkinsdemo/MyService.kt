eb4c50a7cfa50c158ac4eb0e399f6b0f0b785987
CprChandan <chandraprakashr@gmail.com>
2024-11-30 13:08:22 +0530

    Changes in MyService files

--- app/src/main/java/com/cpr/jenkinsdemo/MyService.kt
+++ app/src/main/java/com/cpr/jenkinsdemo/MyService.kt
@@ -3,10 +3,23 @@ package com.cpr.jenkinsdemo
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.widget.Toast
 
 class MyService : Service() {
 
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return onBind(intent)
     }
 }
